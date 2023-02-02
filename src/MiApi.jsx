6bebc86b7113639f1a701401dd6a35b595ae215a
Nandem1 <vicenteignacioac22@gmail.com>
2023-02-02 15:34:14 -0300

    primer commit

---
+++ src/MiApi.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import umbrellaLogo from "./umbrellalogo.png"
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import "./MiApi.css"
+
+const MiApi = () => {
+    const [data, setData] = useState([])
+    const [inputValue, setInputValue] = useState("")
+    const [sortAscending, setSortAscending] = useState(true)
+
+    useEffect(() => {
+        consumeApi()
+    }, [])
+
+    const consumeApi = async () => {
+        try {
+            let response = await fetch("")
+            let database = await response.json()
+            setData(database.data)
+        } catch {
+            alert("No hay respuesta de la API en Github Pages")
+        }
+    }
+
+    const handleInputChange = event => {
+        setInputValue(event.target.value)
+    }
+
+    const handleSortClick = () => {
+        setSortAscending(!sortAscending)
+    }
+
+    const filteredData = data.filter(item => item.nombre.toLowerCase().includes(inputValue.toLowerCase())).sort((a, b) => {
+        if (a.nombre < b.nombre) return sortAscending ? -1 : 1
+        if (a.nombre > b.nombre) return sortAscending ? 1 : -1
+        return 0
+    })
+
+    return (
+        <div>
+            <Navbar bg="light" variant="light" className="border">
+                <Container>
+                    <Navbar.Brand href="#home" className="d-flex justify-content-center align-items-center">
+                        <img
+                            alt=""
+                            src={umbrellaLogo}
+                            width="80"
+                            height="80"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        <p className="head-text">Resident Evil Characters</p>
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="w-100 mt-3 d-flex">
+                            <Button className="me-1" variant="dark" onClick={handleSortClick}>Sort</Button>
+                            <input className="w-100 ms-auto" type="text" value={inputValue} onChange={handleInputChange} placeholder="Buscar por nombre" />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <Container>
+                <Row className="mt-3">
+                    <Col>
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nombre</th>
+                                    <th>Estado actual</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {filteredData.map((item, index) => (<tr>
+                                    <td key={item.id}>{index + 1}</td>
+                                    <td key={item.nombre}>{item.nombre}</td>
+                                    <td key={item.status}>{item.status}</td>
+                                </tr>))}
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default MiApi
---
+++ src/data.js
@@ -0,0 +1,155 @@
+const data = [
+    {
+        "id": 1,
+        "nombre": "Leon Scott Kennedy",
+        "status": "Vivo"
+    },
+    {
+        "id": 2,
+        "nombre": "Ada Wong",
+        "status": "Viva"
+    },
+    {
+        "id": 3,
+        "nombre": "Jill Valentine",
+        "status": "Viva"
+    },
+    {
+        "id": 4,
+        "nombre": "Carlos Olivera",
+        "status": "Vivo"
+    },
+    {
+        "id": 5,
+        "nombre": "Claire Redfield",
+        "status": "Viva"
+    },
+    {
+        "id": 6,
+        "nombre": "Chris Redfield",
+        "status": "Vivo"
+    },
+    {
+        "id": 7,
+        "nombre": "Sherry Birkin",
+        "status": "Viva"
+    },
+    {
+        "id": 8,
+        "nombre": "Ethan Winters",
+        "status": "Fallecido"
+    },
+    {
+        "id": 9,
+        "nombre": "Rebecca Chambers",
+        "status": "Viva"
+    },
+    {
+        "id": 10,
+        "nombre": "Barry Burton",
+        "status": "Vivo"
+    },
+    {
+        "id": 11,
+        "nombre": "Billy Coen",
+        "status": "Desconocido"
+    },
+    {
+        "id": 12,
+        "nombre": "Helena Harper",
+        "status": "Viva"
+    },
+    {
+        "id": 13,
+        "nombre": "Jake Muller",
+        "status": "Vivo"
+    },
+    {
+        "id": 14,
+        "nombre": "Joe Baker",
+        "status": "Vivo"
+    },
+    {
+        "id": 15,
+        "nombre": "Mia Winters",
+        "status": "Viva"
+    },
+    {
+        "id": 16,
+        "nombre": "Moira Burton",
+        "status": "Viva"
+    },
+    {
+        "id": 17,
+        "nombre": "Sheva Alomar",
+        "status": "Viva"
+    },
+    {
+        "id": 18,
+        "nombre": "VECTOR",
+        "status": "Vivo"
+    },
+    {
+        "id": 19,
+        "nombre": "HUNK",
+        "status": "Vivo"
+    },
+    {
+        "id": 20,
+        "nombre": "Albert Wesker",
+        "status": "Fallecido"
+    },
+    {
+        "id": 21,
+        "nombre": "Alcina Dimitrescu",
+        "status": "Fallecida"
+    },
+    {
+        "id": 22,
+        "nombre": "Alex Wesker",
+        "status": "Fallecida"
+    },
+    {
+        "id": 23,
+        "nombre": "Annette Birkin",
+        "status": "Fallecida"
+    },
+    {
+        "id": 24,
+        "nombre": "William Birkin",
+        "status": "Fallecido"
+    },
+    {
+        "id": 25,
+        "nombre": "Ashley Graham",
+        "status": "Viva"
+    },
+    {
+        "id": 26,
+        "nombre": "Brad Vickers",
+        "status": "Fallecido"
+    },
+    {
+        "id": 27,
+        "nombre": "Cassandra Dimitrescu",
+        "status": "Fallecida"
+    },
+    {
+        "id": 28,
+        "nombre": "Eveline",
+        "status": "Fallecida"
+    },
+    {
+        "id": 29,
+        "nombre": "Jack Krauser",
+        "status": "Fallecido"
+    },
+    {
+        "id": 30,
+        "nombre": "Lucas Baker",
+        "status": "Fallecido"
+    },
+
+]
+
+export default data
